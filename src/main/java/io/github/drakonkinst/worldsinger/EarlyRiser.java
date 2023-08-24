6cd028759a6ec36803f7528e2cece17e26e4ad63
Drakonkinst <drakonkinstmc@gmail.com>
2023-08-24 01:11:27 -0700

    Add Fabric-ASM and add custom PathNodeType

---
+++ src/main/java/io/github/drakonkinst/worldsinger/EarlyRiser.java
@@ -0,0 +1,19 @@
+package io.github.drakonkinst.worldsinger;
+
+import com.chocohead.mm.api.ClassTinkerers;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.MappingResolver;
+
+public class EarlyRiser implements Runnable {
+
+    private static final String INTERMEDIARY = "intermediary";
+
+    @Override
+    public void run() {
+        MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
+
+        String nodePathTypeEnum = remapper.mapClassName(INTERMEDIARY, "net.minecraft.class_7");
+        ClassTinkerers.enumBuilder(nodePathTypeEnum, float.class).addEnum("AETHER_SPORE_SEA", -1.0f)
+                .build();
+    }
+}
--- src/main/java/io/github/drakonkinst/worldsinger/mixin/entity/ai/LandPathNodeMakerMixin.java
+++ src/main/java/io/github/drakonkinst/worldsinger/mixin/entity/ai/LandPathNodeMakerMixin.java
@@ -1,5 +1,6 @@
 package io.github.drakonkinst.worldsinger.mixin.entity.ai;
 
+import com.chocohead.mm.api.ClassTinkerers;
 import io.github.drakonkinst.worldsinger.fluid.ModFluidTags;
 import net.minecraft.entity.ai.pathing.LandPathNodeMaker;
 import net.minecraft.entity.ai.pathing.PathNodeType;
@@ -7,6 +8,7 @@ import net.minecraft.fluid.FluidState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockView;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -14,12 +16,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(LandPathNodeMaker.class)
 public abstract class LandPathNodeMakerMixin {
 
+    @Unique
+    private static final PathNodeType AETHER_SPORE_SEA = ClassTinkerers.getEnum(PathNodeType.class,
+            "AETHER_SPORE_SEA");
+
     @Inject(method = "getCommonNodeType", at = @At("TAIL"), cancellable = true)
     private static void avoidSporeBlocks(BlockView world, BlockPos pos,
             CallbackInfoReturnable<PathNodeType> cir) {
         FluidState fluidState = world.getFluidState(pos);
         if (fluidState.isIn(ModFluidTags.AETHER_SPORES)) {
-            cir.setReturnValue(PathNodeType.DAMAGE_OTHER);
+            cir.setReturnValue(AETHER_SPORE_SEA);
         }
     }
 }
