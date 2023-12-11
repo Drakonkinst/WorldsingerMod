52371b31b1320ae35fc66524f7d256939c23a44a
Drakonkinst <drakonkinstmc@gmail.com>
2023-12-11 01:43:47 -0800

    Add more datagen to items

--- src/main/java/io/github/drakonkinst/worldsinger/datagen/WorldsingerModelGenerator.java
+++ src/main/java/io/github/drakonkinst/worldsinger/datagen/WorldsingerModelGenerator.java
@@ -1,11 +1,18 @@
 package io.github.drakonkinst.worldsinger.datagen;
 
 import io.github.drakonkinst.worldsinger.block.ModBlocks;
+import io.github.drakonkinst.worldsinger.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.block.Block;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ItemConvertible;
 
+// Datagen is very limited and only really works for the simplest items and blocks for now.
+// Still, it's worthwhile and saves some work.
 public class WorldsingerModelGenerator extends FabricModelProvider {
 
     public WorldsingerModelGenerator(FabricDataOutput output) {
@@ -14,28 +21,100 @@ public class WorldsingerModelGenerator extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.CRIMSON_GROWTH);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEAD_CRIMSON_GROWTH);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEAD_SPORE_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.DEEPSLATE_SILVER_ORE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MAGMA_VENT);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MIDNIGHT_SPORE_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RAW_SILVER_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.ROSEITE_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.ROSEITE_SPORE_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SALT_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SALTSTONE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SALTSTONE_SALT_ORE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SILVER_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SILVER_ORE);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.STEEL_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SUNLIGHT_SPORE_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.VERDANT_SPORE_BLOCK);
-        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.ZEPHYR_SPORE_BLOCK);
+        registerSimpleCubeBlocks(blockStateModelGenerator, new Block[] {
+                ModBlocks.CRIMSON_GROWTH,
+                ModBlocks.DEAD_CRIMSON_GROWTH,
+                ModBlocks.DEAD_SPORE_BLOCK,
+                ModBlocks.CRIMSON_SPORE_BLOCK,
+                ModBlocks.DEEPSLATE_SILVER_ORE,
+                ModBlocks.MAGMA_VENT,
+                ModBlocks.MIDNIGHT_SPORE_BLOCK,
+                ModBlocks.RAW_SILVER_BLOCK,
+                ModBlocks.ROSEITE_SPORE_BLOCK,
+                ModBlocks.SALT_BLOCK,
+                ModBlocks.SALTSTONE,
+                ModBlocks.SALTSTONE_SALT_ORE,
+                ModBlocks.SILVER_BLOCK,
+                ModBlocks.SILVER_ORE,
+                ModBlocks.STEEL_BLOCK,
+                ModBlocks.SUNLIGHT_SPORE_BLOCK,
+                ModBlocks.VERDANT_SPORE_BLOCK,
+                ModBlocks.ZEPHYR_SPORE_BLOCK
+        });
+        BlockStateModelGenerator.BlockTexturePool roseiteTexturePool = blockStateModelGenerator.registerCubeAllModelTexturePool(
+                ModBlocks.ROSEITE_BLOCK);
+        roseiteTexturePool.stairs(ModBlocks.ROSEITE_STAIRS);
+        roseiteTexturePool.slab(ModBlocks.ROSEITE_SLAB);
+    }
+
+    private void registerSimpleCubeBlocks(BlockStateModelGenerator blockStateModelGenerator,
+            Block[] blocks) {
+        for (Block block : blocks) {
+            blockStateModelGenerator.registerSimpleCubeAll(block);
+        }
     }
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        registerGeneratedItems(itemModelGenerator, new ItemConvertible[] {
+                // Can't do blocks because their path leads to block textures.
+                // ModBlocks.CRIMSON_SNARE,
+                // ModBlocks.DEAD_CRIMSON_SNARE,
+                // ModBlocks.CRIMSON_SPINES,
+                // ModBlocks.DEAD_CRIMSON_SPINES,
+                // ModBlocks.TALL_CRIMSON_SPINES,
+                // ModBlocks.DEAD_TALL_CRIMSON_SPINES,
+                // ModBlocks.VERDANT_VINE_SNARE,
+                // ModBlocks.DEAD_VERDANT_VINE_SNARE,
+                // ModBlocks.TWISTING_VERDANT_VINES,
+                // ModBlocks.DEAD_TWISTING_VERDANT_VINES,
+                ModItems.VERDANT_VINE,
+                ModItems.CRUDE_IRON,
+                ModItems.RAW_SILVER,
+                ModItems.SILVER_NUGGET,
+                ModItems.SILVER_INGOT,
+                ModItems.SALT,
+                ModItems.STEEL_NUGGET,
+                ModItems.STEEL_INGOT,
+                ModItems.DEAD_SPORES_BUCKET,
+                ModItems.VERDANT_SPORES_BUCKET,
+                ModItems.CRIMSON_SPORES_BUCKET,
+                ModItems.ZEPHYR_SPORES_BUCKET,
+                ModItems.SUNLIGHT_SPORES_BUCKET,
+                ModItems.ROSEITE_SPORES_BUCKET,
+                ModItems.MIDNIGHT_SPORES_BUCKET,
+                ModItems.FLINT_AND_IRON,
+                ModItems.QUARTZ_AND_IRON,
+                ModItems.QUARTZ_AND_STEEL
+        });
+        registerHandheldItems(itemModelGenerator, new ItemConvertible[] {
+                ModItems.CRIMSON_SPINE,
+                ModItems.STEEL_AXE,
+                ModItems.STEEL_PICKAXE,
+                ModItems.STEEL_HOE,
+                ModItems.STEEL_SHOVEL,
+                ModItems.STEEL_SWORD
+        });
 
+        // Adds armor trim models as well, which we didn't have before
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.STEEL_HELMET);
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.STEEL_CHESTPLATE);
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.STEEL_LEGGINGS);
+        itemModelGenerator.registerArmor((ArmorItem) ModItems.STEEL_BOOTS);
     }
+
+    private void registerGeneratedItems(ItemModelGenerator itemModelGenerator,
+            ItemConvertible[] items) {
+        for (ItemConvertible item : items) {
+            itemModelGenerator.register(item.asItem(), Models.GENERATED);
+        }
+    }
+
+    private void registerHandheldItems(ItemModelGenerator itemModelGenerator,
+            ItemConvertible[] items) {
+        for (ItemConvertible item : items) {
+            itemModelGenerator.register(item.asItem(), Models.HANDHELD);
+        }
+    }
+
 }
