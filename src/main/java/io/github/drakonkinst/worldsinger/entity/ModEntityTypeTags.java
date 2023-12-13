60cae617ebbaad0f2785dbbdb73cfb244d4f14e1
Drakonkinst <drakonkinstmc@gmail.com>
2023-12-13 13:55:20 -0800

    Move entity type tags

--- src/main/java/io/github/drakonkinst/worldsinger/entity/ModEntityTypeTags.java
+++ src/main/java/io/github/drakonkinst/worldsinger/entity/ModEntityTypeTags.java
@@ -1,21 +1,27 @@
 package io.github.drakonkinst.worldsinger.entity;
 
 import io.github.drakonkinst.worldsinger.Worldsinger;
+import io.github.drakonkinst.worldsinger.util.ModConstants;
 import net.minecraft.entity.EntityType;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 public final class ModEntityTypeTags {
 
-    public static final TagKey<EntityType<?>> SPORES_ALWAYS_AFFECT = ModEntityTypeTags.of(
+    public static final TagKey<EntityType<?>> SPORES_ALWAYS_AFFECT = ModEntityTypeTags.ofCommon(
             "spores_always_affect");
-    public static final TagKey<EntityType<?>> SPORES_NEVER_AFFECT = ModEntityTypeTags.of(
+    public static final TagKey<EntityType<?>> SPORES_NEVER_AFFECT = ModEntityTypeTags.ofCommon(
             "spores_never_affect");
-    public static TagKey<EntityType<?>> HAS_STEEL = ModEntityTypeTags.of("has_steel");
-    public static TagKey<EntityType<?>> HAS_IRON = ModEntityTypeTags.of("has_iron");
+    public static TagKey<EntityType<?>> HAS_STEEL = ModEntityTypeTags.ofCommon("has_steel");
+    public static TagKey<EntityType<?>> HAS_IRON = ModEntityTypeTags.ofCommon("has_iron");
 
     private static TagKey<EntityType<?>> of(String id) {
         return TagKey.of(RegistryKeys.ENTITY_TYPE, Worldsinger.id(id));
+    }
+
+    private static TagKey<EntityType<?>> ofCommon(String id) {
+        return TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(ModConstants.COMMON_ID, id));
     }
 
     private ModEntityTypeTags() {}
