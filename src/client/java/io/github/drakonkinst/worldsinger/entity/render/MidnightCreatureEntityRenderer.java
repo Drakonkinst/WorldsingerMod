30926a070885be9e7513f87e357c61172dc3fe1a
Drakonkinst <drakonkinstmc@gmail.com>
2023-12-17 01:06:05 -0800

    Move shapeshifting rendering to a more generic class
    In case Midnight Creatures aren't the only shapeshifting entity,
    make a more generic class and have the MidnightCreatureEntityRenderer
    customize itself based on this.

--- src/client/java/io/github/drakonkinst/worldsinger/entity/render/MidnightCreatureEntityRenderer.java
+++ src/client/java/io/github/drakonkinst/worldsinger/entity/render/MidnightCreatureEntityRenderer.java
@@ -3,29 +3,16 @@ package io.github.drakonkinst.worldsinger.entity.render;
 import io.github.drakonkinst.worldsinger.Worldsinger;
 import io.github.drakonkinst.worldsinger.entity.MidnightCreatureEntity;
 import io.github.drakonkinst.worldsinger.entity.model.MidnightCreatureEntityModel;
-import io.github.drakonkinst.worldsinger.mixin.accessor.EntityAccessor;
-import io.github.drakonkinst.worldsinger.mixin.accessor.LimbAnimatorAccessor;
-import io.github.drakonkinst.worldsinger.mixin.accessor.LivingEntityAccessor;
 import io.github.drakonkinst.worldsinger.registry.ModEntityRenderers;
 import io.github.drakonkinst.worldsinger.util.ColorUtil;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.OverlayTexture;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.entity.EntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.LimbAnimator;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.entity.passive.SchoolingFishEntity;
-import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
 
 public class MidnightCreatureEntityRenderer extends
-        MobEntityRenderer<MidnightCreatureEntity, MidnightCreatureEntityModel> {
+        ShapeshiftingEntityRenderer<MidnightCreatureEntity, MidnightCreatureEntityModel> {
 
     public static final int MIDNIGHT_OVERLAY_COLOR = ColorUtil.colorToInt(0, 0, 0, 251);
     public static final int MIDNIGHT_OVERLAY_UV = OverlayTexture.packUv(0, 0);
@@ -37,84 +24,14 @@ public class MidnightCreatureEntityRenderer extends
                 context.getPart(ModEntityRenderers.MODEL_MIDNIGHT_CREATURE_LAYER)), 0.5f);
     }
 
+    // Always render fish upright
     @Override
-    public Identifier getTexture(MidnightCreatureEntity entity) {
-        return Worldsinger.id("textures/entity/midnight_creature/midnight_creature.png");
+    protected boolean shouldBeTouchingWater(MidnightCreatureEntity entity, LivingEntity morph) {
+        return super.shouldBeTouchingWater(entity, morph) || morph instanceof SchoolingFishEntity;
     }
 
     @Override
-    public void render(MidnightCreatureEntity entity, float f, float tickDelta,
-            MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
-        LivingEntity morph = entity.getMorph();
-        if (morph == null) {
-            super.render(entity, f, tickDelta, matrixStack, vertexConsumerProvider, i);
-            return;
-        }
-
-        // Sync limbs
-        LimbAnimator target = morph.limbAnimator;
-        LimbAnimator source = entity.limbAnimator;
-        target.setSpeed(source.getSpeed());
-        ((LimbAnimatorAccessor) target).worldsinger$setPrevSpeed(
-                ((LimbAnimatorAccessor) source).worldsinger$getPrevSpeed());
-        ((LimbAnimatorAccessor) target).worldsinger$setPos(source.getPos());
-
-        // Sync data
-        morph.handSwinging = entity.handSwinging;
-        morph.handSwingTicks = entity.handSwingTicks;
-        morph.lastHandSwingProgress = entity.lastHandSwingProgress;
-        morph.handSwingProgress = entity.handSwingProgress;
-        morph.bodyYaw = entity.bodyYaw;
-        morph.prevBodyYaw = entity.prevBodyYaw;
-        morph.headYaw = entity.headYaw;
-        morph.prevHeadYaw = entity.prevHeadYaw;
-        morph.age = entity.age;
-        morph.preferredHand = entity.preferredHand;
-        morph.deathTime = entity.deathTime;
-        morph.hurtTime = entity.hurtTime;
-        morph.setOnGround(entity.isOnGround());
-        morph.setVelocity(entity.getVelocity());
-
-        ((EntityAccessor) morph).worldsinger$setVehicle(entity.getVehicle());
-        ((EntityAccessor) morph).worldsinger$setTouchingWater(
-                entity.isTouchingWater() || morph instanceof SchoolingFishEntity);
-
-        // Pitch for Phantoms is inverted
-        if (morph instanceof PhantomEntity) {
-            morph.setPitch(-entity.getPitch());
-            morph.prevPitch = -entity.prevPitch;
-        } else {
-            morph.setPitch(entity.getPitch());
-            morph.prevPitch = entity.prevPitch;
-        }
-
-        // Equip held items and armor
-        // Note: Should not run these if using visual equipment
-        morph.equipStack(EquipmentSlot.MAINHAND, entity.getEquippedStack(EquipmentSlot.MAINHAND));
-        morph.equipStack(EquipmentSlot.OFFHAND, entity.getEquippedStack(EquipmentSlot.OFFHAND));
-        morph.equipStack(EquipmentSlot.HEAD, entity.getEquippedStack(EquipmentSlot.HEAD));
-        morph.equipStack(EquipmentSlot.CHEST, entity.getEquippedStack(EquipmentSlot.CHEST));
-        morph.equipStack(EquipmentSlot.LEGS, entity.getEquippedStack(EquipmentSlot.LEGS));
-        morph.equipStack(EquipmentSlot.FEET, entity.getEquippedStack(EquipmentSlot.FEET));
-
-        if (morph instanceof MobEntity) {
-            ((MobEntity) morph).setAttacking(entity.isAttacking());
-        }
-
-        // Assign pose
-        morph.setPose(entity.getPose());
-
-        // Set active hand after configuring held items
-        morph.setCurrentHand(
-                entity.getActiveHand() == null ? Hand.MAIN_HAND : entity.getActiveHand());
-        ((LivingEntityAccessor) morph).worldsinger$setLivingFlag(1, entity.isUsingItem());
-        morph.getItemUseTime();
-        ((LivingEntityAccessor) morph).worldsinger$tickActiveItemStack();
-
-        // Render
-        EntityRenderer<? super LivingEntity> morphRenderer = MinecraftClient.getInstance()
-                .getEntityRenderDispatcher()
-                .getRenderer(morph);
-        morphRenderer.render(morph, f, tickDelta, matrixStack, vertexConsumerProvider, i);
+    public Identifier getTexture(MidnightCreatureEntity entity) {
+        return Worldsinger.id("textures/entity/midnight_creature/midnight_creature.png");
     }
 }
--- src/client/java/io/github/drakonkinst/worldsinger/entity/render/ShapeshiftingEntityRenderer.java
+++ src/client/java/io/github/drakonkinst/worldsinger/entity/render/ShapeshiftingEntityRenderer.java
@@ -1,2 +1,110 @@
-package io.github.drakonkinst.worldsinger.entity.render;public class ShapeshiftingEntityRenderer {
+package io.github.drakonkinst.worldsinger.entity.render;
+
+import io.github.drakonkinst.worldsinger.entity.ShapeshiftingEntity;
+import io.github.drakonkinst.worldsinger.mixin.accessor.EntityAccessor;
+import io.github.drakonkinst.worldsinger.mixin.accessor.LimbAnimatorAccessor;
+import io.github.drakonkinst.worldsinger.mixin.accessor.LivingEntityAccessor;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory.Context;
+import net.minecraft.client.render.entity.MobEntityRenderer;
+import net.minecraft.client.render.entity.model.EntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LimbAnimator;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PhantomEntity;
+import net.minecraft.util.Hand;
+
+public abstract class ShapeshiftingEntityRenderer<T extends ShapeshiftingEntity, M extends EntityModel<T>> extends
+        MobEntityRenderer<T, M> {
+
+    public ShapeshiftingEntityRenderer(Context context, M entityModel, float f) {
+        super(context, entityModel, f);
+    }
+
+    protected void configureEquipment(T entity, LivingEntity morph) {
+        // Equip held items and armor
+        // Note: Should not run these if using visual equipment
+        morph.equipStack(EquipmentSlot.MAINHAND, entity.getEquippedStack(EquipmentSlot.MAINHAND));
+        morph.equipStack(EquipmentSlot.OFFHAND, entity.getEquippedStack(EquipmentSlot.OFFHAND));
+        morph.equipStack(EquipmentSlot.HEAD, entity.getEquippedStack(EquipmentSlot.HEAD));
+        morph.equipStack(EquipmentSlot.CHEST, entity.getEquippedStack(EquipmentSlot.CHEST));
+        morph.equipStack(EquipmentSlot.LEGS, entity.getEquippedStack(EquipmentSlot.LEGS));
+        morph.equipStack(EquipmentSlot.FEET, entity.getEquippedStack(EquipmentSlot.FEET));
+    }
+
+    protected boolean shouldBeTouchingWater(T entity, LivingEntity morph) {
+        return entity.isTouchingWater();
+    }
+
+    @Override
+    public void render(T entity, float f, float tickDelta, MatrixStack matrixStack,
+            VertexConsumerProvider vertexConsumerProvider, int i) {
+        LivingEntity morph = entity.getMorph();
+        if (morph == null) {
+            super.render(entity, f, tickDelta, matrixStack, vertexConsumerProvider, i);
+            return;
+        }
+
+        // Sync limbs
+        LimbAnimator target = morph.limbAnimator;
+        LimbAnimator source = entity.limbAnimator;
+        target.setSpeed(source.getSpeed());
+        ((LimbAnimatorAccessor) target).worldsinger$setPrevSpeed(
+                ((LimbAnimatorAccessor) source).worldsinger$getPrevSpeed());
+        ((LimbAnimatorAccessor) target).worldsinger$setPos(source.getPos());
+
+        // Sync data
+        morph.handSwinging = entity.handSwinging;
+        morph.handSwingTicks = entity.handSwingTicks;
+        morph.lastHandSwingProgress = entity.lastHandSwingProgress;
+        morph.handSwingProgress = entity.handSwingProgress;
+        morph.bodyYaw = entity.bodyYaw;
+        morph.prevBodyYaw = entity.prevBodyYaw;
+        morph.headYaw = entity.headYaw;
+        morph.prevHeadYaw = entity.prevHeadYaw;
+        morph.age = entity.age;
+        morph.preferredHand = entity.preferredHand;
+        morph.deathTime = entity.deathTime;
+        morph.hurtTime = entity.hurtTime;
+        morph.setOnGround(entity.isOnGround());
+        morph.setVelocity(entity.getVelocity());
+
+        ((EntityAccessor) morph).worldsinger$setVehicle(entity.getVehicle());
+        ((EntityAccessor) morph).worldsinger$setTouchingWater(shouldBeTouchingWater(entity, morph));
+
+        // Pitch for Phantoms is inverted
+        if (morph instanceof PhantomEntity) {
+            morph.setPitch(-entity.getPitch());
+            morph.prevPitch = -entity.prevPitch;
+        } else {
+            morph.setPitch(entity.getPitch());
+            morph.prevPitch = entity.prevPitch;
+        }
+
+        configureEquipment(entity, morph);
+
+        if (morph instanceof MobEntity) {
+            ((MobEntity) morph).setAttacking(entity.isAttacking());
+        }
+
+        // Assign pose
+        morph.setPose(entity.getPose());
+
+        // Set active hand after configuring held items
+        morph.setCurrentHand(
+                entity.getActiveHand() == null ? Hand.MAIN_HAND : entity.getActiveHand());
+        ((LivingEntityAccessor) morph).worldsinger$setLivingFlag(1, entity.isUsingItem());
+        morph.getItemUseTime();
+        ((LivingEntityAccessor) morph).worldsinger$tickActiveItemStack();
+
+        // Render
+        EntityRenderer<? super LivingEntity> morphRenderer = MinecraftClient.getInstance()
+                .getEntityRenderDispatcher()
+                .getRenderer(morph);
+        morphRenderer.render(morph, f, tickDelta, matrixStack, vertexConsumerProvider, i);
+    }
 }
